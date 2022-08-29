94c9089ae93a92da26429da8b619be7ca47beff9
yumi <1136366578@qq.com>
2022-08-29 13:48:31 +0800

    完成了商品列表页面的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,76 @@
+<template>
+  <view class="goodsItem">
+    <!-- 左侧的盒子 -->
+    <view class="goodsListLeft">
+      <image class="goodsPic" :src="item.goods_small_logo || defaultPic"></image>
+    </view>
+    <!-- 右侧的盒子 -->
+    <view class="goodsListRight">
+      <!-- 商品名字 -->
+      <view class="goodsName">
+        {{ item.goods_name }}
+      </view>
+      <view class="goodsInfoBox">
+        <!-- 商品价格 -->
+        <view class="goodsPrice">￥{{item.goods_price | tofixed}}</view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      }
+    },
+    filters:{
+      tofixed(num){
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goodsItem {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .goodsListLeft {
+      margin-right: 5px;
+
+      .goodsPic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goodsListRight {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goodsName {
+        font-size: 13px;
+      }
+
+      .goodsInfoBox {
+        .goodsPrice {
+          color: #C00000;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,22 +1,115 @@
 <template>
-	<view>
-		goods_list
-	</view>
+  <view>
+    <view class="goodsList">
+      <view v-for="(item,index) in goodsList" :key="index" @click="gotoDetail(item)">
+        <my-goods :item="item"></my-goods>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+  export default {
+    data() {
+      return {
+        // 请求参数对象
+        queryObj: {
+          // 查询关键字
+          query: '',
+          // 分类ID
+          cid: '',
+          // 页面索引
+          pagenum: 1,
+          // 每一页长度
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0,
+        // 节流阀：是否正在请求数据
+        isLoading: false
+      }
+    },
+    onLoad(options) {
+      // console.log(options)
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      // 调用获取商品列表数据的方法
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表
+      async getGoodsList(cb) {
+        // 打开节流阀
+        this.isLoading = true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // console.log(res)
+        this.isLoading = false
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg('获取商品信息失败')
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      gotoDetail(item) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+        // console.log('点击了')
+      }
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('没有更多数据了')
+      if (this.isLoading) return
+      // 让页码值自增+1
+      this.queryObj.pagenum += 1
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      // 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isLoading = false
+      this.goodsList = []
+      // 重新发起数据请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
+    }
+  }
 </script>
 
-<style>
+<style lang="scss">
+  .goodsItem {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
 
+    .goodsListLeft {
+      margin-right: 5px;
+
+      .goodsPic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goodsListRight {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .goodsName {
+        font-size: 13px;
+      }
+
+      .goodsInfoBox {
+        .goodsPrice {
+          color: #C00000;
+          font-size: 16px;
+        }
+      }
+    }
+  }
 </style>
